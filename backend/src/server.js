0916bd6197ea94bf9f3a5ac44e6edc5691e41194
Ayush Singhal <ayushsinghal304@gmail.com>
2025-11-07 16:21:32 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,7 @@
 import express from "express"
 import { ENV } from "./lib/env.js"
-import path from "path"
 
 const app = express()
-
-const __dirname = path.resolve()
 
 app.get("/health",(req,res)=>{
     res.status(200).json({
@@ -12,14 +9,6 @@ app.get("/health",(req,res)=>{
     })
 })
 
-// make our app ready for deployment
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
 
 app.listen(ENV.PORT,()=>{
     console.log("server is running on port:",ENV.PORT);
