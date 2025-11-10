04acea55d6a702665c8e98d967634aa7d372d070
Ayush Singhal <ayushsinghal304@gmail.com>
2025-11-10 14:28:43 +0530

    deplo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,34 @@
 import express from "express"
+import cors from "cors"
+import path from "path"
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js"
+import {serve} from "inngest/express"
+import { inngest, functions } from "./lib/inngest.js"
 
 const app = express()
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cors());
+// app.use(cors({origin: ENV.CLIENT_URL,credentials: true}));
+
+app.use("/api/inngest",serve({client:inngest , functions}))
 
 app.get("/health",(req,res)=>{
     res.status(200).json({
         msg: "success from api"
     })
 })
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 const startServer = async()=>{
     try {
