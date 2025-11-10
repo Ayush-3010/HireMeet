0fffc07f0062c6e495f5530579683552780a7e96
Ayush Singhal <ayushsinghal304@gmail.com>
2025-11-10 23:36:11 +0530

    few changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
                     </div>
                     <div className="flex flex-col ">
                         <span className="font-black text-xl bg-linear-to-r from-primary via-secondary to-accent bg-clip-text text-transparent font-mono tracking-wider">
-                            Hire Meet
+                            HireMeet
                         </span>
                         <span className="text-xs text-base-content/60 font-medium -mt-1">
                             Code Together
