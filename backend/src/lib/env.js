26a4f3409f3c6a3130fffc8dd26e5d63dea8b28c
Ayush Singhal <ayushsinghal304@gmail.com>
2025-11-19 20:22:23 +0530

    cors

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -14,5 +14,6 @@ export const ENV={
     STREAM_API_SECRET: process.env.STREAM_API_SECRET,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    CLIENT_URL: process.env.CLIENT_URL
+    CLIENT_URL: process.env.CLIENT_URL,
+    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN
 }
